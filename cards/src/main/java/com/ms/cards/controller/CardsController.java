6ed8be3252f40370f9a359a48a30329c5986e664
hounangogogo <1294030303@qq.com>
2021-08-28 15:49:38 -0700

    added config

--- cards/src/main/java/com/ms/cards/controller/CardsController.java
+++ cards/src/main/java/com/ms/cards/controller/CardsController.java
@@ -1,10 +1,16 @@
 package com.ms.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ms.cards.cofig.CardsServiceConfig;
 import com.ms.cards.model.Cards;
 import com.ms.cards.model.Customer;
+import com.ms.cards.model.Properties;
 import com.ms.cards.service.CardsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +24,22 @@ public class CardsController {
     @Autowired
     private CardsService cardsService;
 
+    @Autowired
+    private CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
         log.info("getCardDetails() method started");
         return cardsService.getCardDetails(customer.getCustomerId());
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
